34b229bf907fd8e473e77dd8c3ba20005ad1e1c2
Akhilanand Prasad <akhilanand.prasad@kongsbergdigital.com>
2023-02-18 00:06:22 +0530

    Extended with new Tailwind theme

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1 className="text-3xl font-bold underline">
+    <h1 className="text-3xl font-bold underline color text-blue">
       Hello world!
     </h1>
   )
