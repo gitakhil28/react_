709b3b1c61cb03da330f072d0ef93f499b2529ee
Akhilanand Prasad <akhilanand.prasad@kongsbergdigital.com>
2023-02-17 23:37:36 +0530

    ✨Setup Tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <h2>Welcome</h2>
-    </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
 
